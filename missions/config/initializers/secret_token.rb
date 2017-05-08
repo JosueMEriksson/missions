23f3bffb72abaa2d4ad1da8e2b5b87574c05eb9b
Josue Martinez Eriksson <josueme1992@hotmail.es>
2017-05-08 17:26:57 +0200

    Crea la carpeta de proyecto Rails

---
+++ missions/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Missions::Application.config.secret_token = '714ad3a8385754cfd4e7d8888ac39e0788ef45bdf61b340edf76f0ffe19b2cad5f021823923b50e3e78cdd47b280e246e3fed68b4757cba3d57689b7e3d016fe'
